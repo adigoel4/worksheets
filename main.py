54bf173305202c7acb29531d9ff3fe548a134398
Adi Goel <aditya.goel@berkeley.edu>
2022-11-17 16:58:32 -0800

    template-testing

--- main.py
+++ main.py
@@ -8,12 +8,39 @@ from formatter import formatter
 load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
+topic = input("What should the quiz be about?\n")
+prompt = f"""Generate a quiz about {topic}. 
+Here are a few examples: 
 
-prompt = input("prompt?\n")
+Fractions Quiz: 
 
+Question 1: What is 1/2 + 1/4? 
+Question 2: If it takes Bob 10 minutes to run 1 mile, how long does it take for him to run 2/5 of 1 mile? 
+Question 3: What is 7/2 * 10/23? 
+
+Answer key: 
+
+Question 1: 3/4 
+Question 2: 4 minutes 
+Question 3: 70/46
+
+Multiplication Quiz: 
+
+Question 1: What is 7 * 8? 
+Question 2: There are 10 girls and 5 guys in the class. If each girl gets 2 apples and each guy gets 1 apple, how many apples are there? 
+Question 3: What is 8 * 1.5? 
+
+Answer key: 
+
+Question 1: 56
+Question 2: 25 apples
+Question 3: 12 
+
+"""
 response = openai.Completion.create(
     engine="text-davinci-001", 
-    prompt=generate_prompt(prompt),
+    prompt= prompt, 
     max_tokens= 500
 )
 gentext = response.choices[0].text
+print(gentext)
